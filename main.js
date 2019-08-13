19db938951a240f05428a0913f0350fee7f80f6b
KYEBOKWAK <bogame@naver.com>
2019-08-13 19:45:49 +0900

    포트ㅜ변경

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ app.post('/abc', function (req, res) {
       usia: '뭐냐'});
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
 /*
