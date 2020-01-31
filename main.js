974111935953e02dfc6c7465e5324cba705c3f2c
KYEBOKWAK <bogame@naver.com>
2020-01-31 17:06:20 +0900

    no message

--- main.js
+++ main.js
@@ -927,14 +927,14 @@ app.post("/test", function(req, res){
    
 });
 
-/*
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-*/
-app.listen(3000, "0.0.0.0", function () {
-  console.log('Example app listening on port 3000!');
-})
+
+// app.listen(3000, "0.0.0.0", function () {
+//   console.log('Example app listening on port 3000!');
+// })
 
 /*
 var http = require('http');
