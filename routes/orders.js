8e446701bdba60c6ab4cfc0ee8877c0521e35178
GyeBo Gwak <bogame@naver.com>
2020-12-11 15:09:06 +0900

    콘텐츠 전달시 문자 보내기 추가

--- routes/orders.js
+++ routes/orders.js
@@ -1911,6 +1911,62 @@ sendStoreApproveSMSOrderUser = (store_order_id) => {
   */
 }
 
+sendStoreRelayCustomerSMSOrderUser = (store_order_id) => {
+
+  const querySelect = mysql.format("SELECT item.title AS item_title, orders_item.contact, orders_item.name AS customer_name, store.title AS creator_name FROM orders_items AS orders_item LEFT JOIN stores AS store ON orders_item.store_id=store.id LEFT JOIN items AS item ON orders_item.item_id=item.id WHERE orders_item.id=?", store_order_id);
+
+  db.SELECT(querySelect, {}, (result) => {
+    if(!result || result.length === 0){
+      return;
+    }
+    
+    const data = result[0];
+    if(!data.contact || data.contact === ''){
+        return;
+    }
+
+    const content = `[크티] 주문하신 콘텐츠가 도착했습니다!  '나의 콘텐츠' 에서 확인해보세요!`;
+    Global_Func.sendSMS(data.contact, content, (result) => {
+
+    })
+
+    // let date = moment_timezone().format('YYYY-MM-DD HH:mm');
+
+    
+    // Global_Func.sendKakaoAlimTalk({
+    //   templateCode: 'CTSTORE02',
+    //   to: data.contact,
+    //   creator_name: data.creator_name,
+    //   item_title: data.item_title,
+    //   approved_at: date,
+    //   customer_name: data.customer_name,
+    // })
+  })
+
+  /*
+  const querySelect = mysql.format("SELECT contact FROM orders_items WHERE id=?", store_order_id);
+
+  db.SELECT(querySelect, {}, (result) => {
+    if(!result || result.length === 0){
+      return;
+    }
+    
+    const data = result[0];
+    if(!data.contact || data.contact === ''){
+        return;
+    }
+
+    
+    const content = '[크티] 크리에이터가 요청된 콘텐츠 준비를 시작했습니다! 상세내용은 웹사이트에서 확인하세요.';
+    Global_Func.sendSMS(data.contact, content, (result) => {
+
+    })
+    
+
+  })
+  */
+}
+
 router.post("/store/state/ok", function(req, res){
   const store_order_id = req.body.data.store_order_id;
 
@@ -1972,6 +2028,12 @@ router.post("/store/state/relay/customer", function(req, res){
 
   db.UPDATE("UPDATE orders_items SET product_answer=?, state=? WHERE id=?", [product_answer, types.order.ORDER_STATE_APP_STORE_RELAY_CUSTOMER, store_order_id], 
   (result) => {
+
+    if(process.env.APP_TYPE !== 'local'){
+      // this.sendStoreApproveEmail(store_order_id);
+      this.sendStoreRelayCustomerSMSOrderUser(store_order_id);
+    }
+
     return res.json({
       result: {
         state: res_state.success,
