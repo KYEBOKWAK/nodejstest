04311aa4404e4c50437cd7b14a40822371d5e2f9
GyeBo Gwak <bogame@naver.com>
2021-06-01 16:35:46 +0900

    매거진 개선

--- routes/magazine.js
+++ routes/magazine.js
@@ -91,5 +91,17 @@ router.post("/any/list", function(req, res){
   });
 });
 
+router.post("/any/list/hidden", function(req, res){
+  const querySelect = mysql.format("SELECT id, title, subtitle, thumb_img_url FROM magazines WHERE is_open=? ORDER BY id DESC", [false]);
+  db.SELECT(querySelect, {}, (result) => {
+    return res.json({
+      result: {
+        state: res_state.success,
+        list: result
+      }
+    })
+  });
+});
+
 
 module.exports = router;
