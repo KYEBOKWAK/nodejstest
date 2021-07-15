38ad545bb94e2aa630ef281ad6898cb66f0ae480
GyeBo Gwak <bogame@naver.com>
2021-07-15 17:23:24 +0900

    테스트 시간 변경

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const TIME_DUE_ONE_TO_ONE_PLAYING_WAIT_CHECK_FIRST_DAY = 2; //1:1 승인 후 2�
 const TIME_DUE_ONE_TO_ONE_PLAYING_WAIT_CHECK_SECOND_DAY = 7;  //1:1 승인 후 7일뒤에 진행 요청 알림
 
 const ALARM_DOWNLOAD_ORDER_CHECK_HOUR = 17;
-const ALARM_DOWNLOAD_ORDER_CHECK_MIN = 19;
+const ALARM_DOWNLOAD_ORDER_CHECK_MIN = 24;
 
 var express = require('express');
 var app = express();
@@ -2515,7 +2515,7 @@ function setTagHOT(){
 }
 
 function alarmDownloadOrderCheck() {
-  const nowDate = moment_timezone().format(`YYYY-MM-DD ${ALARM_DOWNLOAD_ORDER_CHECK_HOUR}:${ALARM_DOWNLOAD_ORDER_CHECK_MIN}:00`);
+  const nowDate = moment_timezone(`2021-07-16 ${ALARM_DOWNLOAD_ORDER_CHECK_HOUR}:${ALARM_DOWNLOAD_ORDER_CHECK_MIN}:00`).format(`YYYY-MM-DD ${ALARM_DOWNLOAD_ORDER_CHECK_HOUR}:${ALARM_DOWNLOAD_ORDER_CHECK_MIN}:00`);
 
   const startDate = moment_timezone(nowDate).add(-1, 'days').format(`YYYY-MM-DD ${ALARM_DOWNLOAD_ORDER_CHECK_HOUR}:${ALARM_DOWNLOAD_ORDER_CHECK_MIN}:00`);
 
