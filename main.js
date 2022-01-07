4e52796e72b9a3ba3cb86b9da398706267701e78
GyeBo Gwak <bogame@naver.com>
2022-01-07 14:45:04 +0900

    ip4 로 인식되게 하기 위함

--- main.js
+++ main.js
@@ -2464,7 +2464,7 @@ app.post('/filedownload/token/make', function(req, res){
   });
 })
 
-app.listen(3000, function () {
+app.listen(3000, '0.0.0.0', function () {
   console.log('Example app listening on port 3000!');
 });
 
