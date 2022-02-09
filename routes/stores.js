28074755cd634debccd1d3d2cf73339e24a31477
GyeBo Gwak <bogame@naver.com>
2022-02-09 18:28:47 +0900

    코드 원복

--- routes/stores.js
+++ routes/stores.js
@@ -2243,8 +2243,8 @@ router.post("/manager/payment/info/v1", function(req, res){
   const store_id = req.body.data.store_id;
   const sort_state = req.body.data.sort_state;
 
-  // const nowDate = moment_timezone();
-  const nowDate = moment_timezone('2022-03-01 00:00:00');
+  const nowDate = moment_timezone();
+  // const nowDate = moment_timezone('2022-03-01 00:00:00');
   //범위 구하기
   let startDate = '';
   let endDate = '';
